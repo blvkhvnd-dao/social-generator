818202854760a5cc9d11c8e3049084d0cf23540c
Daniel Öhrlund <danielohrlund@gmail.com>
2023-11-17 11:12:42 +0100

    only render img in preview if a theme is selected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,9 @@ const Preview = React.forwardRef<HTMLCanvasElement, { banner: AcolyteBanner }>(
           style={{ display: "none" }}
         />
         <div className="block relative aspect-[3/1] w-full md:w-500px lg:w-750px bg-blvk-gray">
-          <img src={banner.theme?.imgPath} alt="banner-preview" />
+          {banner.theme?.imgPath && (
+            <img src={banner.theme?.imgPath} alt="banner-preview" />
+          )}
           <div className="absolute top-0 left-0 right-0 bottom-0 text-center text-white align-middle">
             <div className="flex w-full h-full items-center justify-center">
               <span
